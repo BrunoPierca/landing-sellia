41af21d87da839d4f11e218aee69bed73a9c833f
brunoPierca <pierca18@gmail.com>
2022-11-08 21:32:21 -0300

    Bug fix

--- src/Components/Blog.jsx
+++ src/Components/Blog.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Button, Col, Container, Row } from "react-bootstrap";
-import blogScreen from "../images/blogScreen.png";
 
 export const Blog = () => {
 	return (
