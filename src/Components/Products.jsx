cc15d9b072b982cfc5b88cb94dbf5928489d85f8
brunoPierca <pierca18@gmail.com>
2022-11-17 20:08:46 -0300

    Grid conversion + responsive fixes

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -2,63 +2,51 @@ import { Col, Row } from "react-bootstrap";
 
 export const Products = () => {
 	return (
-		<section id='products' className='ms-md-5 icons-background'>
-			<Row className=' py-5'>
-				<Col sm={4}>
-					<h2>Un producto para cada necesidad</h2>
-					<Col sm={12}>
-						<Row className='g-2'>
-							<Col sm={6}>
+		<div className=' my-5'>
+			<section id='products' className='icons-background'>
+				<Row className='my-5'>
+					<Col sm={12} lg={4} className='pt-5 '>
+						<h4 className='text-center w-75 mx-auto pb-5 fw-600'>Un producto para cada necesidad</h4>
+						<Col sm={12}>
+							<div className='products-grid mx-3 mx-lg-5'>
 								<div className='products-grid-item d-flex justify-content-center align-items-center'>
-									<p className='fw-700 darkBlue m-0'>Franquicias</p>
+									<p className='fw-700 dark-blue py-4 '>Franquicias</p>
 								</div>
-							</Col>
-							<Col sm={6}>
 								<div className='products-grid-item  d-flex justify-content-center align-items-center'>
-									<p className='fw-700 darkBlue m-0'>Enterprise</p>
+									<p className='fw-700 dark-blue py-4 '>Enterprise</p>
 								</div>
-							</Col>
-						</Row>
-						<Row className='g-2'>
-							<Col sm={6}>
 								<div className='products-grid-item d-flex justify-content-center align-items-center'>
-									<p className='fw-700 darkBlue m-0'>Trivias</p>
+									<p className='fw-700 dark-blue py-4 '>Trivias</p>
 								</div>
-							</Col>
-							<Col sm={6}>
 								<div className='products-grid-item d-flex justify-content-center align-items-center'>
-									<p className='fw-700 darkBlue m-0'>RRHH</p>
+									<p className='fw-700 dark-blue py-4'>RRHH</p>
 								</div>
+								<div className='d-flex full-width-product mb-4 justify-content-center align-items-center'>
+									<p className='fw-700 dark-blue py-4'>Hazlo a tu medida!</p>
+								</div>
+							</div>
+						</Col>
+					</Col>
+					<Col sm={12} lg={8} id='products-column' className='background-bone'>
+						<Row>
+							<Col sm={7} className='text-center mt-5'>
+								<h3 className='fw-700 dark-blue my-3'>Productos</h3>
+								<p className='fw-500 mx-2'>
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc, amet donec morbi vitae semper at vulputate. Elementum
+									vitae, mattis orci ultrices scelerisque sed. Magna praesent lectus quisque diam vivamus sed. Amet, egestas malesuada
+									tortor augue pellentesque. Ut aliquam blandit vulputate pulvinar ipsum risus. Nulla neque augue et tempor viverra odio
+									facilisi sed. Interdum lorem urna augue in posuere ut quis id commodo. Arcu orci, neque et elementum. Placerat lacus est
+									lobortis ut. Aliquet in a et pulvinar eu rhoncus at quis. Venenatis, diam diam in sed integer. Aliquet at luctus
+									pellentesque blandit dignissim ipsum.
+								</p>
 							</Col>
-						</Row>
-						<Row className='justify-content-between'>
-							<Col sm={12}>
-								<div className='d-flex products-grid-item justify-content-center align-items-center'>
-									<p className='fw-700 darkBlue m-0'>Hazlo a tu medida</p>
-								</div>
+							<Col sm={4} className='mt-5 justify-content-center d-flex pb-5'>
+								<img src='/images/selliaPhone.png' className='img-fluid ' alt='Sellia phone' />
 							</Col>
 						</Row>
 					</Col>
-				</Col>
-				<Col sm={8} id='products-column'>
-					<Row>
-						<Col sm={7} className='text-center mt-5'>
-							<h2 className='fw-700 darkBlue my-3'>Productos</h2>
-							<p className='fw-500 '>
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc, amet donec morbi vitae semper at vulputate. Elementum vitae,
-								mattis orci ultrices scelerisque sed. Magna praesent lectus quisque diam vivamus sed. Amet, egestas malesuada tortor augue
-								pellentesque. Ut aliquam blandit vulputate pulvinar ipsum risus. Nulla neque augue et tempor viverra odio facilisi sed.
-								Interdum lorem urna augue in posuere ut quis id commodo. Arcu orci, neque et elementum. Placerat lacus est lobortis ut.
-								Aliquet in a et pulvinar eu rhoncus at quis. Venenatis, diam diam in sed integer. Aliquet at luctus pellentesque blandit
-								dignissim ipsum.
-							</p>
-						</Col>
-						<Col sm={4} className='mt-5'>
-							<img src='/images/selliaPhone.png' className='img-fluid ' alt='Sellia phone' />
-						</Col>
-					</Row>
-				</Col>
-			</Row>
-		</section>
+				</Row>
+			</section>
+		</div>
 	);
 };
