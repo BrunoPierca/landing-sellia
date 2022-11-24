8f5128e9ec6b7aaa84f0ef9f30341b0ad293239b
brunoPierca <pierca18@gmail.com>
2022-11-24 20:09:33 -0300

    Changed animation on features to fire on hover

--- src/Components/FeatureCard.jsx
+++ src/Components/FeatureCard.jsx
@@ -3,19 +3,19 @@ import ReactCardFlip from "react-card-flip";
 
 export const FeatureCard = ({ feature, description, icon }) => {
 	const [isFlipped, setIsFlipped] = useState(false);
-	const handleClick = (e) => {
+	const handleHover = (e) => {
 		e.preventDefault();
 		setIsFlipped(!isFlipped);
 	};
 
 	return (
 		<ReactCardFlip isFlipped={isFlipped} flipDirection='horizontal'>
-			<div key='front' onClick={handleClick} className='features-grid-item h-100 py-3 justify-content-center align-items-center'>
+			<div key='front' onMouseEnter={handleHover} className='features-grid-item h-100 py-3 justify-content-center align-items-center'>
 				{icon}
 				<p className='w-100 fw-500 mt-1'>{feature}</p>
 			</div>
 
-			<div key='back' onClick={handleClick} className='features-grid-item h-100 py-3 justify-content-center align-items-center'>
+			<div key='back' onMouseLeave={handleHover} className='features-grid-item h-100 py-3 justify-content-center align-items-center'>
 				{description}
 			</div>
 		</ReactCardFlip>
