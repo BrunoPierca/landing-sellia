9535b70a7731caffe7c0c950c9ffafd4c03c8a90
brunoPierca <pierca18@gmail.com>
2022-11-25 16:18:36 -0300

    Added hero animation

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -1,13 +1,25 @@
+import { useState } from "react";
 import { Container } from "react-bootstrap";
 import { AboutUs, Blog, Clients, ContactUs, DemoButton, Features, Footer, Hero, NavBar, Products, Support } from "./Components";
 
 export const LandingPage = () => {
+	const [bgPosition, setBgPosition] = useState("0 0");
+	const handleMouseMove = (e) => {
+		const x = -e.clientX / 15;
+		const y = -e.clientY / 15;
+		setBgPosition(x + "px " + y + "px");
+	};
+
 	return (
 		<>
 			<NavBar />
 
 			<Container className='g-0' fluid>
-				<div className='extended-background icons-background'>
+				<div
+					onMouseMove={(e) => handleMouseMove(e)}
+					style={{ backgroundPosition: bgPosition }}
+					className='extended-background icons-background'
+				>
 					<Hero />
 					<Clients />
 				</div>
