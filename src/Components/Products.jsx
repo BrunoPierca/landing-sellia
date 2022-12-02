6dcd7a567f8b69a2910ce6e493f58299710b5aa6
brunoPierca <pierca18@gmail.com>
2022-12-01 23:02:45 -0300

    Added products animation

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -1,6 +1,17 @@
+import { useState } from "react";
 import { Col, Row } from "react-bootstrap";
 
 export const Products = () => {
+	let initialActiveItem = "Franquicias";
+	const [activeBackground, setActiveBackground] = useState(`url("../images/Sellia_${initialActiveItem}.gif")`);
+	const handleBackgroundChange = (product, { target }) => {
+		for (let i = 0; i < target.parentNode.childNodes.length; i++) {
+			const element = target.parentNode.childNodes[i];
+			element.classList.remove("active-item");
+		}
+		setActiveBackground(`url("../images/Sellia_${product}.gif")`);
+		target.classList.add("active-item");
+	};
 	return (
 		<div className='py-5 extended-background icons-background'>
 			<section id='products'>
@@ -9,19 +20,34 @@ export const Products = () => {
 						<h4 className='text-center w-75 mx-auto pb-5 fw-600'>Un producto para cada necesidad</h4>
 						<Col sm={12}>
 							<div className='products-grid mx-3 mx-lg-5'>
-								<div className='products-grid-item d-flex justify-content-center align-items-center'>
+								<div
+									onClick={(e) => handleBackgroundChange("Franquicias", e)}
+									className='products-grid-item d-flex justify-content-center active-item align-items-center'
+								>
 									<p className='fw-700 dark-blue py-4 my-3'>Franquicias</p>
 								</div>
-								<div className='products-grid-item  d-flex justify-content-center align-items-center'>
+								<div
+									onClick={(e) => handleBackgroundChange("Enterprise", e)}
+									className='products-grid-item  d-flex justify-content-center align-items-center'
+								>
 									<p className='fw-700 dark-blue py-4 my-3'>Enterprise</p>
 								</div>
-								<div className='products-grid-item d-flex justify-content-center align-items-center'>
+								<div
+									onClick={(e) => handleBackgroundChange("Trivias", e)}
+									className='products-grid-item d-flex justify-content-center align-items-center'
+								>
 									<p className='fw-700 dark-blue py-4 my-3'>Trivias</p>
 								</div>
-								<div className='products-grid-item d-flex justify-content-center align-items-center'>
+								<div
+									onClick={(e) => handleBackgroundChange("RRHH", e)}
+									className='products-grid-item d-flex justify-content-center align-items-center'
+								>
 									<p className='fw-700 dark-blue py-4 my-3'>RRHH</p>
 								</div>
-								<div className='d-flex full-width-product mb-4 justify-content-center align-items-center'>
+								<div
+									onClick={(e) => handleBackgroundChange("AMedida", e)}
+									className='d-flex full-width-product mb-4 justify-content-center align-items-center'
+								>
 									<p className='fw-700 dark-blue py-4 my-3'>Hazlo a tu medida!</p>
 								</div>
 							</div>
@@ -41,9 +67,11 @@ export const Products = () => {
 								</p>
 							</Col>
 							<Col sm={4} className='mt-5 justify-content-center d-flex pb-5'>
-								<div className='phone-container'>
-									<img src='./images/selliaPhoneNobg.png' className='img-fluid phone ' alt='Sellia phone' />
-									<img src='./images/Sellia_Franquicias.gif' className='img-fluid phone-content' alt='Sellia phone content' />
+								{/* animate__animated animate__shakeY */}
+								<div className='phone-container  ' style={{ backgroundImage: activeBackground }}>
+									<span className='phone-mockup'></span>
+									{/* <img src='./images/selliaPhoneNobg.png' className='img-fluid phone ' alt='Sellia phone' />
+									<img src='./images/Sellia_Franquicias.gif' className='img-fluid phone-content' alt='Sellia phone content' /> */}
 								</div>
 							</Col>
 						</Row>
